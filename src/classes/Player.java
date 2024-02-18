f469886c4b65634b692a2c33710f638471c4b9b9
Helon21 <helonbentes51@hotmail.com>
2024-02-18 15:59:36 -0300

    Feat/Creating world and player

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,5 +1,35 @@
 package classes;
 
-public class Player {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Player extends Rectangle{
+
+    public boolean right, up, left, down;
+    public int speed = 4;
+
+    public Player(int x, int y){
+        super(x, y, 32,32);
+    }
     
+    public void tick(){
+        if(right && World.isFree(x+speed, y)){
+            x+=speed;
+        }else if(left && World.isFree(x-speed, y)){
+            x-=speed;
+        }
+
+        if(up && World.isFree(x, y-speed)){
+            y-=speed;
+        }else if(down && World.isFree(x, y+speed)){
+            y+=speed;
+        }
+    }
+
+    public void render(Graphics g){
+        g.setColor(Color.blue);
+        g.fillRect(x, y, width, height);
+    }
+
 }
